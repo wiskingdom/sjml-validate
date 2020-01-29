9d0e9e04316c1142c4fb28db19765eac924e879c
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-29 10:45:39 +0900

    Test converter

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const xToJ = require('fast-xml-parser');
-var Ajv = require('ajv');
-var ajv = new Ajv(); 
+const Ajv = require('ajv');
+const ajv = new Ajv(); 
 
 const { xEscape, toX } = require('./lib/xTools.js');
 
 
-const schema = JSON.parse(fs.readFileSync('jsonschema/wr.json', 'utf8'));
-var validate = ajv.compile(schema);
-const xml = fs.readFileSync('wr/data2.sjml', 'utf8');
+const schema = JSON.parse(fs.readFileSync('schema/wr.json', 'utf8'));
+const validate = ajv.compile(schema);
+const xml = fs.readFileSync('sample/wr/data2.sjml', 'utf8');
 const escapedXml = xEscape(xml);
 
 try {
@@ -19,13 +19,13 @@ try {
 
 try {
   var obj = xToJ.parse(escapedXml, {}, true);
-  const p = obj['SJML']['text'].split(/[\n\r]+/)
-  .map(str => str.replace(/ +/g, ' ').trim());
-  obj['SJML']['text'] = { p };
+  //const p = obj['SJML']['text'].split(/[\n\r]+/)
+  //.map(str => str.replace(/ +/g, ' ').trim());
+  //obj['SJML']['text'] = { p };
 
 fs.writeFileSync('data2.json', JSON.stringify(obj, null, 2));
 fs.writeFileSync('data2.sjml', toX('wr')(obj));
-var valid = validate(obj);
+const valid = validate(obj);
 if (!valid) {
   console.log(validate.errors);
 }
