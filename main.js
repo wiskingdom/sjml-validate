899ccf69910453882324f72951a3e7394b88fc78
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-31 04:18:53 +0900

    Modulize

--- main.js
+++ main.js
@@ -1,44 +1,66 @@
 const fs = require('fs');
 const path = require('path');
-const { xRunConvert } = require('./lib/xTools');
+const { xRun, xLog } = require('./lib/xTools');
 
-//options
-// souceType: wr:원문, wm:원문-잡지, wn:뉴스, ww:웹, sd:일상대화, ss:구어
-const schemaType = 'wr';
-const extention = '.sjml';
-const inputFolder = path
-  .normalize('sample/WXOR');
-const outputFolder = path
-  .normalize('output/WXOR');
-const parentFolder = path.join(...outputFolder.split(path.sep).slice(0, -1));
-const folderName = inputFolder.split(path.sep).reverse()[0];
-const errLogPath = path.join(parentFolder, `errLog_${folderName}.txt`);
-const validLogPath = path.join(parentFolder, `validLog_${folderName}.txt`);
-const tokenSizePath = path.join(parentFolder, `tokenSize_${folderName}.txt`);
+// functions
+const getPassFail = bool => bool ? 'pass' : 'fail';
 
-const fileNames = fs.readdirSync(inputFolder).filter(fileName => fileName.endsWith(extention));
-const report = {
-  parseLog: [],
-  validLog: [],
-  tokenSizeLog: []
+const main = ({ runType, schemaType, inputFolder, extFilter, outputFolder}) => {
+  const fileNames = fs.readdirSync(inputFolder)
+    .filter(fileName => fileName.endsWith(extFilter));
+  const report = {
+    parseLog: [],
+    validLog: [],
+    processResult: [],
+    tokenSizeResult: []
+  }
+
+  fileNames.forEach(fileName => {
+    const inputFilePath = path.join(inputFolder, fileName);
+    const {
+      passEncoding,
+      passParse,
+      passValid,
+      parseLog,
+      validLog,
+      tokenSize,
+      outputFileName,
+      outputXml,
+    } = xRun({ runType, schemaType, inputFilePath, outputFolder });
+
+    const resultFileName = outputFileName || fileName;
+    const tokenSizeResult = [resultFileName, tokenSize].join('\t');
+    const processResult = [passEncoding, passParse, passValid]
+      .map(getPassFail).join('\t');
+
+    report.tokenSizeResult.push(tokenSizeResult);
+    report.processResult.push(processResult);
+    report.parseLog.push(parseLog);
+    report.validLog.push(validLog);
+
+    if (parseLog) console.log(parseLog);
+    if (validLog) console.log(validLog);
+    if (runType === 'convert') {
+      const resultFolder = path.join(outputFolder, schemaType);
+      const resultFilePath = path
+        .join(resultFolder, `${path.parse(resultFileName).name}.sjml`);
+      fs.mkdirSync(resultFolder, { recursive: true });
+      fs.writeFileSync(resultFilePath, outputXml);
+    }
+  });
+  xLog({ outputFolder, runType, schemaType, report });
 }
 
-fileNames.forEach(fileName => {
-  const filePath = path.join(inputFolder, fileName);
-  const xmlPath = path.join(outputFolder, fileName.replace(extention, '.sjml'));
-  const sourceXml = fs.readFileSync(filePath, 'utf8');
-  const result = xRunConvert(schemaType)(filePath)(sourceXml);
-  fs.writeFileSync(xmlPath, result.targetXml);
-  if (result.parseLog) console.log(result.parseLog);
-  if (result.validLog) console.log(result.validLog);
-  report.parseLog.push(result.parseLog);
-  report.validLog.push(result.validLog);
-  report.tokenSizeLog.push(result.tokenSizeLog);
-});
+// options
+// souceType: WXRW:문어, WCRW:문어-잡지, NXRW:뉴스, ww:웹, sd:일상대화, ss:구어
+const schemaType = 'WXRW';
+const extFilter = '.sjml';
+const runType = 'convert';
+const inputFolder = path
+  .normalize('sample/WXRW');
+const outputFolder = path
+  .normalize('output');
 
-const serialize = arr => arr.join('\n').replace(/[\n\r]+/g, '\n').trim();
-
-fs.writeFileSync(errLogPath, serialize(report.parseLog));
-fs.writeFileSync(validLogPath, serialize(report.validLog));
-fs.writeFileSync(tokenSizePath, report.tokenSizeLog.join('\n'));
+// main
+main({ runType, schemaType, inputFolder, extFilter, outputFolder });
 
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
+const chardet = require('chardet');
 const obj = { a: '12', b: '34'};
 
 console.log({ ...obj, a:'85' });
 
 const arr = ['abx'];
 console.log(arr.join('\n'));
+
+const encoding = chardet.detectFileSync('sample/enc.sjml', { sampleSize: 256 });
+
+console.log(encoding);
