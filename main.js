97ce3148720d1643ed21acf779f56a2002542ddf
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-30 21:52:00 +0900

    Modulize

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const { xValidate } = require('./lib/xTools.js');
+const { xRunConvert } = require('./lib/xTools');
 
 //options
 // souceType: wr:원문, wm:원문-잡지, wn:뉴스, ww:웹, sd:일상대화, ss:구어
-const sourceType = 'wnn';
+const schemaType = 'wr';
 const extention = '.sjml';
 const inputFolder = path
-  .normalize('C:\\Users\\korean\\Desktop\\검증기사자료\\corpus');
+  .normalize('sample/WXOR');
 const outputFolder = path
-  .normalize('C:\\Users\\korean\\Desktop\\검증기사자료re\\corpus');
+  .normalize('output/WXOR');
 const parentFolder = path.join(...outputFolder.split(path.sep).slice(0, -1));
 const folderName = inputFolder.split(path.sep).reverse()[0];
 const errLogPath = path.join(parentFolder, `errLog_${folderName}.txt`);
@@ -18,24 +18,27 @@ const tokenSizePath = path.join(parentFolder, `tokenSize_${folderName}.txt`);
 
 const fileNames = fs.readdirSync(inputFolder).filter(fileName => fileName.endsWith(extention));
 const report = {
-  errLog: [],
+  parseLog: [],
   validLog: [],
-  tokenSize: []
+  tokenSizeLog: []
 }
 
 fileNames.forEach(fileName => {
   const filePath = path.join(inputFolder, fileName);
   const xmlPath = path.join(outputFolder, fileName.replace(extention, '.sjml'));
-  const result = xValidate(sourceType)(filePath);
-  fs.writeFileSync(xmlPath, result.xml);
-  report.errLog.push(result.errLog.join('\n'));
-  report.validLog.push(result.validLog.join('\n'));
-  report.tokenSize.push(result.tokenSize);
+  const sourceXml = fs.readFileSync(filePath, 'utf8');
+  const result = xRunConvert(schemaType)(filePath)(sourceXml);
+  fs.writeFileSync(xmlPath, result.targetXml);
+  if (result.parseLog) console.log(result.parseLog);
+  if (result.validLog) console.log(result.validLog);
+  report.parseLog.push(result.parseLog);
+  report.validLog.push(result.validLog);
+  report.tokenSizeLog.push(result.tokenSizeLog);
 });
 
 const serialize = arr => arr.join('\n').replace(/[\n\r]+/g, '\n').trim();
 
-fs.writeFileSync(errLogPath, serialize(report.errLog));
+fs.writeFileSync(errLogPath, serialize(report.parseLog));
 fs.writeFileSync(validLogPath, serialize(report.validLog));
-fs.writeFileSync(tokenSizePath, report.tokenSize.join('\n'));
+fs.writeFileSync(tokenSizePath, report.tokenSizeLog.join('\n'));
 
---
+++ test.js
@@ -0,0 +1,6 @@
+const obj = { a: '12', b: '34'};
+
+console.log({ ...obj, a:'85' });
+
+const arr = ['abx'];
+console.log(arr.join('\n'));
