eab4884462ae5276cbb597ee962cf05ae3d4966d
Yeonwoo Kim <wiskingdom@gmail.com>
2020-02-12 03:36:55 +0900

    Shake temp files

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-const xToJ = require('fast-xml-parser');
-const chalk = require('chalk');
+const path = require('path');
+const { xsMain } = require('./lib/xsMain.js');
 
-const parseOptions = {
-  attributeNamePrefix : 'att_',
-  ignoreAttributes : false,
-  parseNodeValue : false,
-  trimValues: false,
-};
+// options
+// souceType: WXRW:문어, WCRW:문어-잡지, NXRW:뉴스
+const schemaType = 'WXRW';
+const extFilter = 'sjml';
+const runType = 'convert';
+const inputFolder = path
+  .normalize('C:\\Users\\yeon\\Desktop\\sample\\WXOR');
+const outputFolder = path
+  .normalize('C:\\Users\\yeon\\Desktop\\output');
 
-
+// main
+xsMain({ runType, schemaType, inputFolder, extFilter, outputFolder });
