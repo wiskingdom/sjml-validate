401137696fdec1d410f791846229f5732c4c1f77
Yeonwoo Kim <wiskingdom@gmail.com>
2020-02-14 03:18:58 +0900

    Publish v0.1.9

--- run.js
+++ run.js
@@ -2,7 +2,6 @@ const path = require('path');
 const { xsMain } = require('./lib/xsMain.js');
 
 // options
-// souceType: WXRW:문어, WCRW:문어-잡지, NXRW:뉴스
 const schemaType = 'SXRW';
 const extFilter = 'sjml';
 const runType = 'validate';
