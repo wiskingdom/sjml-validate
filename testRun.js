143135b5b08b165f1d175ccf92a55f5ebc372a28
Yeonwoo Kim <wiskingdom@gmail.com>
2020-02-21 11:25:34 +0900

    Change parseLog

--- testRun.js
+++ testRun.js
@@ -2,13 +2,13 @@ const path = require('path');
 const { xsMain } = require('./lib/xsMain.js');
 
 // options
-const schemaType = 'SXRW';
+const schemaType = 'EXRW';
 const extFilter = 'sjml';
 const runType = 'validate';
 const inputFolder = path
-  .normalize('C:\\Users\\yeon\\Desktop\\sample\\SXRW');
+  .normalize('C:\\Users\\korean\\Desktop\\EXRW');
 const outputFolder = path
-  .normalize('C:\\Users\\yeon\\Desktop\\output');
+  .normalize('C:\\Users\\korean\\Desktop\\output');
 
 // main
 xsMain({ runType, schemaType, inputFolder, extFilter, outputFolder });
